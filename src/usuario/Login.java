78a513834b4934e4cceee5b32fc68b545ec923f6
Vitor Malevichi <vmalevichi@gmail.com>
2026-09-17 21:47:38 -0300

    Cadastro no Login
    Inverter as classes depois

--- src/usuario/Login.java
+++ src/usuario/Login.java
@@ -1,21 +1,32 @@
 package usuario;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Login {
-    
+        Usuario usuario;
     public Login(){
 
     }
-
+    
     public Boolean verificaLogin(){
         Scanner sc = new Scanner(System.in);
+        System.out.println("Insira seu nome: ");
         String nome = sc.nextLine();
+        System.out.println("Insira seu CPF: ");
         String cpf = sc.nextLine();
+        System.out.println("Insira sua data de nascimento: ");
+        String dataNasc = sc.nextLine();
+        System.out.println("Insira seu telefone: ");
         int telefone = sc.nextInt();
+        System.out.println("Insira sua senha: ");
         String senha = sc.nextLine();
 
-        Usuario usuario = new Usuario(nome, cpf, telefone, senha);
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        LocalDate data = LocalDate.parse(dataNasc, formatador);
+
+        usuario = new Usuario(nome, cpf, data, telefone, senha);
         return true;
     }
 }
